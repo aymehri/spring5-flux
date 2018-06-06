21f6c7c6b6ca026db1c61cf9e35a381b31416881
Aymen Mehri <aymen.mehri@soat.fr>
2018-06-06 18:01:13 +0200

    first commit

---
+++ src/main/java/com/aymen/kadi/Spring5FluxApplication.java
@@ -0,0 +1,12 @@
+package com.aymen.kadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5FluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5FluxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aymen/kadi/Spring5FluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aymen.kadi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5FluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
